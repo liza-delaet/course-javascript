888dab304f15d51cc9db76306027fb58faf3aa13
Elizaveta Gorn <liza.delaet.target@gmail.com>
2022-11-18 15:55:49 +0300

    Homework 2

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,14 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr[i] = fn(array[i], i, array);
+  }
+
+  return newArr;
+}
 
 /*
  Задание 3:
@@ -31,7 +42,21 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let temp;
+  let i;
+  if (initial === undefined) {
+    temp = array[0];
+    i = 1;
+  } else {
+    temp = initial;
+    i = 0;
+  }
+  for (; i < array.length; i++) {
+    temp = fn(temp, array[i], i, array);
+  }
+  return temp;
+}
 
 /*
  Задание 4:
@@ -41,7 +66,14 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const props = [];
+  for (const key in obj) {
+    props.push(key.toUpperCase());
+  }
+
+  return props;
+}
 
 /*
  Задание 5 *:
@@ -54,6 +86,13 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set(target, prop, value) {
+      target[prop] = value ** 2;
+      return true;
+    },
+  });
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
